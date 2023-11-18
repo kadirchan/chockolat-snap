aaacdf824af12637fc4720be6547f849903845e8
KADIRCAN BOZKURT <e2448207@ceng.metu.edu.tr>
2023-11-18 17:38:56 +0300

    fckd up

--- packages/site/.cache/page-ssr/routes/render-page.js
+++ packages/site/.cache/page-ssr/routes/render-page.js
@@ -13122,24 +13122,24 @@ function y(){return(y=Object.assign||function(e){for(var t=1;t<arguments.length;
 /***/ }),
 
 /***/ "react-dom/server":
-/*!*****************************************************************************************************************************!*\
-  !*** external "/home/kadircan/Desktop/chockolat-snap/transaction-insights-snaps-workshop/node_modules/react-dom/server.js" ***!
-  \*****************************************************************************************************************************/
+/*!****************************************************************************************************!*\
+  !*** external "/home/kadircan/Desktop/ethglobal/chockolat-snap2/node_modules/react-dom/server.js" ***!
+  \****************************************************************************************************/
 /***/ ((module) => {
 
 "use strict";
-module.exports = require("/home/kadircan/Desktop/chockolat-snap/transaction-insights-snaps-workshop/node_modules/react-dom/server.js");
+module.exports = require("/home/kadircan/Desktop/ethglobal/chockolat-snap2/node_modules/react-dom/server.js");
 
 /***/ }),
 
 /***/ "react":
-/*!************************************************************************************************************************!*\
-  !*** external "/home/kadircan/Desktop/chockolat-snap/transaction-insights-snaps-workshop/node_modules/react/index.js" ***!
-  \************************************************************************************************************************/
+/*!***********************************************************************************************!*\
+  !*** external "/home/kadircan/Desktop/ethglobal/chockolat-snap2/node_modules/react/index.js" ***!
+  \***********************************************************************************************/
 /***/ ((module) => {
 
 "use strict";
-module.exports = require("/home/kadircan/Desktop/chockolat-snap/transaction-insights-snaps-workshop/node_modules/react/index.js");
+module.exports = require("/home/kadircan/Desktop/ethglobal/chockolat-snap2/node_modules/react/index.js");
 
 /***/ }),
 
--- packages/site/.cache/sync-requires.js
+++ packages/site/.cache/sync-requires.js
@@ -4,7 +4,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/home/kadircan/Desktop/chockolat-snap/transaction-insights-snaps-workshop/packages/site/.cache/dev-404-page.js")),
-  "component---src-pages-index-tsx": preferDefault(require("/home/kadircan/Desktop/chockolat-snap/transaction-insights-snaps-workshop/packages/site/src/pages/index.tsx"))
+  "component---cache-dev-404-page-js": preferDefault(require("/home/kadircan/Desktop/ethglobal/chockolat-snap2/packages/site/.cache/dev-404-page.js")),
+  "component---src-pages-index-tsx": preferDefault(require("/home/kadircan/Desktop/ethglobal/chockolat-snap2/packages/site/src/pages/index.tsx"))
 }
 
